910a11cbe07bf712f9fce51d7dd9f186a048c737
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2024-08-02 17:55:38 +0800

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,4 @@
 import { MuxAsyncIterator } from "./mod.ts";
-import type { Logger } from "./mod.ts";
 
 type HttpsConfig = {
   port?: number | undefined;
@@ -11,37 +10,16 @@ type HttpConfig = {
   port?: number | undefined;
 } | undefined;
 
-type LoggerOption = {
-  mode: "disable" | "console" | "file" | "both";
-  level: "info" | "warn" | "error";
-  dir: string;
-  rotate: boolean;
-  maxBytes?: number | undefined;
-  maxBackupCount?: number | undefined;
-};
-
 export async function configure(
   http: HttpConfig,
   https: HttpsConfig,
-  logger: Logger,
-  loggerOption: LoggerOption,
   bind: string,
 ) {
-  if (loggerOption.mode === "both" || loggerOption.mode === "file") {
-    const { maxBackupCount, maxBytes, rotate } = loggerOption;
-    await logger.initFileLogger(loggerOption.dir, {
-      maxBackupCount,
-      maxBytes,
-      rotate,
-    });
-  }
-
   let httpServer;
   let httpsServer;
   if (http?.port !== undefined) {
     httpServer = Deno.listen({ port: http.port, hostname: bind });
     const addr = <Deno.NetAddr> httpServer.addr;
-    logger.info(`HTTP server is listening on ${addr.hostname}:${addr.port}`);
   }
 
   if (https) {
@@ -53,56 +31,11 @@ export async function configure(
       ]);
       httpsServer = Deno.listenTls({ port, cert: c, key: k, hostname: bind });
       const addr = <Deno.NetAddr> httpsServer.addr;
-      logger.info(`HTTPS server is listening on ${addr.hostname}:${addr.port}`);
     }
-  }
-
-  logger.disable();
-
-  switch (loggerOption.mode) {
-    case "disable":
-      logger.disableConsole();
-      logger.disableFile();
-      break;
-
-    case "file":
-      setLogLevel(logger, loggerOption.level);
-      logger.disableConsole();
-      logger.enableFile();
-      break;
-
-    case "console":
-      setLogLevel(logger, loggerOption.level);
-      logger.enableConsole();
-      logger.disableFile();
-      break;
-
-    case "both":
-      setLogLevel(logger, loggerOption.level);
-      logger.enableConsole();
-      logger.enableFile;
-      break;
   }
   const listener = new MuxAsyncIterator<Deno.Conn>();
   httpServer && listener.add(httpServer);
   httpsServer && listener.add(httpsServer);
 
   return listener;
-}
-
-function setLogLevel(logger: Logger, level: "info" | "warn" | "error") {
-  switch (level) {
-    case "info":
-      logger.enable("info");
-      logger.enable("warn");
-      logger.enable("error");
-      break;
-    case "warn":
-      logger.enable("warn");
-      logger.enable("error");
-      break;
-    case "error":
-      logger.enable("error");
-      break;
-  }
 }
